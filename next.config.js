c170e361228c639e43aea810960293892eb80cf8
rajae-amr <109186572+rajae-amr@users.noreply.github.com>
2024-11-25 00:48:07 +0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -7,7 +7,10 @@ const nextConfig = {
     NEXT_PUBLIC_APP_URL: process.env.VERCEL_URL 
       ? `https://${process.env.VERCEL_URL}` 
       : 'http://localhost:3000'
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
